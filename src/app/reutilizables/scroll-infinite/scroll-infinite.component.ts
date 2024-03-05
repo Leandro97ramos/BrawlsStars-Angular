19abce24127def4789fdf4553df7d77254e78006
Leandro97ramos <leandro.97.ramos@gmail.com>
2024-03-05 10:17:48 -0300

    :art: Styles fixed

--- src/app/reutilizables/scroll-infinite/scroll-infinite.component.ts
+++ src/app/reutilizables/scroll-infinite/scroll-infinite.component.ts
@@ -1,6 +1,5 @@
-import { Component, HostListener, ElementRef, ViewChild, Input, OnChanges, SimpleChanges, AfterViewInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, AfterViewInit, EventEmitter, Output } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
-import { Observable, of } from 'rxjs';
 
 
 @Component({
@@ -8,24 +7,27 @@ import { Observable, of } from 'rxjs';
   templateUrl: './scroll-infinite.component.html',
   styleUrls: ['./scroll-infinite.component.css']
 })
-export class ScrollInfiniteComponent implements OnChanges,AfterViewInit  {
+export class ScrollInfiniteComponent implements OnChanges, AfterViewInit {
 
   @Input() data: any = {};
   @Input() route: string;
   @Input() allFilters: string[] = [];
-  @Output() filterSelected = new EventEmitter<string>(); 
-  
+  @Output() filterSelected = new EventEmitter<string>();
+
   dataIndexed: any = {};
 
-  tagFilter:any = [];
+  tagFilter: any = [];
 
   isDataReady: boolean = false;
+
+  isMobile: boolean;
+
 
 
   styleClassMap: { [key: string]: string } = {
     'brawlers': 'brawlers-style',
     'maps': 'maps-style',
-    
+
   };
 
   constructor() {
@@ -34,80 +36,95 @@ export class ScrollInfiniteComponent implements OnChanges,AfterViewInit  {
     this.tagFilter = [];
     this.allFilters = [];
     this.route = '';
-     
+    this.isMobile = false;
   }
 
-  /*Carrousel*/
-customOptions: OwlOptions = {
-  loop: true,
-  mouseDrag: true,
-  touchDrag: true,
-  pullDrag: true,
-  dots: true,
-  navSpeed: 700,
-  navText: ['', ''],
-  responsive: {
-    90: {
-      items: 1
-    },
-    100: {
-      items: 2
-    },
-    101: {
-      items: 3
-    }
-  },
-  nav: true
-}
+
+  ngOnInit(): void {
+    this.isMobile = this.checkIfMobile();
+    window.addEventListener('resize', this.onResize.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize(): void {
+    this.isMobile = this.checkIfMobile();
+  }
+
+  checkIfMobile(): boolean {
+    return window.innerWidth < 575; // Cambia este valor según el tamaño de pantalla que desees
+  }
 
 
+  /*Tag Carrousel*/
+  tagOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      10: {
+        items: 2
+      },
+      20: {
+        items: 3
+      }
+    },
+    nav: true
+  }
 
 
 
   
+
+
   ngAfterViewInit(): void {
     //recargar el carrousel
-    
+
   }
-  
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['data']) {
       //console.log('Datos recibidos en app-scroll-infinite:', this.data);
       this.tagFilter = [...new Set(this.data.map((item: any) => this.allFilters.filter))];
-      this.tagFilter.unshift('All');    
+      this.tagFilter.unshift('All');
       //console.log('Filter:', this.allFilters);
-     // this.getItemsMaps();
-  
+      // this.getItemsMaps();
+      console.log('Datos recibidos en app-scroll-infinite:', this.data);
+
 
     }
     //console.log('Datos recibidos en tags:', this.tagFilter);
   }
-  
+
   filtrar = (tag: string): void => {
-   
 
 
-   
+
+
   }
 
   onFilterClick(filter: string): void {
     this.filterSelected.next(filter);
 
     console.log('Filtrar por:', filter);
-   
-    
+
+
   }
 
-get isDataAvailable(): boolean {
-  return this.data.length > 0;
-}
+  get isDataAvailable(): boolean {
+    return this.data.length > 0;
+  }
 
 
 }
 
 
-/*
-
-
-
-*/
